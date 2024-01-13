359c3f2851bd2807577ab96bd064f415b97de77e
Node-Prophet <85714933+Carlito-Pedida@users.noreply.github.com>
2024-01-12 19:32:20 -0600

    Update GeoLocator.js

--- src/Components/GeoLocator.js
+++ src/Components/GeoLocator.js
@@ -4,7 +4,7 @@
 // import { geolocator } from "geolocator";
 
 // const accessToken =
-//   "pk.eyJ1IjoiY2FybHBlZDQxNyIsImEiOiJjbHI1c2E1MWgyMXZtMmtxbWUzc21tMmFwIn0.yXjsy5SB4bfSPytcQchNAg";
+
 
 // const MapComponent = () => {
 //   const [viewport, setViewport] = useState({
